e955da3d71004331d320b0833198d2dd335619be
amirgal <amirc538@gmail.com>
2020-02-05 18:13:56 +0200

    fixed last user button

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let lastUser = {}
 renderer.renderDropDownMenu(JSON.parse(localStorage.users || "{}"))
 
 $('#loadData').on('click', function() {
-    // localStorage.lastUser = JSON.stringify(data)
     lastUser = {...data}
     apiManager.loadData()
 })
@@ -26,17 +25,11 @@ $('#saveUserPage').on('click', function () {
 $('#loadUserPage').on('click', function () {      
     const userId = $('#drop-down').children("option:selected").data().id 
     if(userId == undefined) {return}  
-    // localStorage.lastUser = JSON.stringify(data)
     lastUser = {...data}
     renderer.renderPage(JSON.parse(localStorage.users)[userId])
 })
 
 $('.user-container').on('click','#loadLastUser', function() {
-    // if($.isEmptyObject(JSON.parse(localStorage.lastUser))){return}
-    // renderer.renderPage(JSON.parse(localStorage.lastUser))
-
     if($.isEmptyObject(lastUser)){return}
     renderer.renderPage(lastUser)
-
-    // data = JSON.parse(localStorage.lastUser)
 })
